5bf9ed583f0a1022ba0aea444cdc31d11696022d
Xunze Tan <24247472@studentmail.ul.ie>
2026-02-12 21:51:21 +0000

    restructure to multi-module microservices

---
+++ notification-service/src/test/java/com/bidhub/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.bidhub.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
